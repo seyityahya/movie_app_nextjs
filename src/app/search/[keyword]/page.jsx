010b150987743cc95d80987625249281c8efc1e9
seyityahya <seyityahya05@gmail.com>
2023-05-06 16:15:56 +0300

    finish project

---
+++ src/app/search/[keyword]/page.jsx
@@ -0,0 +1,27 @@
+import Movies from "@/components/Movies";
+import React from "react";
+
+const Page = async ({ params }) => {
+  const keyword = params.keyword;
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${keyword}&language=en-US&include_adult=false`
+  );
+  const data = await res.json();
+
+  return (
+    <div>
+      {!data?.results ? (
+        <div>Aranılan şey bulunamadı !!!</div>
+      ) : (
+        <div className="flex items-center flex-wrap gap-3">
+          {data?.results?.map((dt, i) => (
+            <Movies key={i} dt={dt} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Page;
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -6,25 +6,29 @@ import React from "react";
 const Movies = ({ dt }) => {
   const router = useRouter();
   return (
-    <div
-      onClick={() => router.push(`/movie/${dt?.id}`)}
-      className="min-w-[470] relative imgContainer cursor-pointer"
-    >
-      <Image
-        style={{ objectFit: "cover" }}
-        width={470}
-        height={300}
-        src={`https://image.tmdb.org/t/p/original/${
-          dt?.backdrop_path || dt?.poster_path
-        }`}
-      />
-      <div className="absolute bottom-0 p-3 w-full h-full flex flex-col justify-end opacity-0 hover:opacity-100 transition-opacity">
-        <div className="text-2xl font-bold">{dt?.title}</div>
-        <div>
-          {dt?.release_date} - {dt?.vote_average}
+    <>
+      {dt.backdrop_path ? (
+        <div
+          onClick={() => router.push(`/movie/${dt?.id}`)}
+          className="min-w-[470] relative imgContainer cursor-pointer"
+        >
+          <Image
+            style={{ objectFit: "cover" }}
+            width={470}
+            height={300}
+            src={`https://image.tmdb.org/t/p/original/${dt?.backdrop_path}`}
+          />
+          <div className="absolute bottom-0 p-3 w-full h-full flex flex-col justify-end opacity-0 hover:opacity-100 transition-opacity">
+            <div className="text-2xl font-bold">{dt?.title}</div>
+            <div>
+              {dt?.release_date} - {dt?.vote_average}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        ""
+      )}
+    </>
   );
 };
 
